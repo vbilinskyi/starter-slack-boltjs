f884d50403eb7f26f99b812966696e56433365fe
Vitalii Bilinskyi <bilinskyivitalii@gmail.com>
2022-11-11 14:00:18 +0200

    socket mode disable

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const app = new App({
   receiver,
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  appToken: process.env.SLACK_APP_TOKEN,
 });
 
 app.event("app_home_opened", async ({say}) => {
